5dae2a21a475610ac32a61d282cbc52b604271d9
HeyBanditoz <banditoz@protonmail.com>
2019-08-12 23:10:42 -0600

    Format memory usage to one decimal place

--- src/main/java/io/banditoz/gmecord/tasks/ActivityTimerTask.java
+++ src/main/java/io/banditoz/gmecord/tasks/ActivityTimerTask.java
@@ -4,17 +4,20 @@ import io.banditoz.gmecord.Bot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Activity;
 
+import java.text.DecimalFormat;
 import java.util.TimerTask;
 
 public class ActivityTimerTask extends TimerTask {
     @Override
     public void run() {
         JDA jda = Bot.getJda();
+        DecimalFormat df = new DecimalFormat(".#"); // round to one decimal place
+
         long responseTime = jda.getGatewayPing();
         Runtime runtime = Runtime.getRuntime();
-        long memUsed = (runtime.totalMemory() - runtime.freeMemory()) / (1024 * 1024);
-        long memTotal = runtime.maxMemory() / (1024 * 1024);
-        Activity activity = Activity.playing(responseTime + " ms - " + memUsed + " MB / " + memTotal + " MB");
+        double memUsed = (runtime.totalMemory() - runtime.freeMemory()) / (1024.0 * 1024.0);
+        double memTotal = runtime.maxMemory() / (1024.0 * 1024.0);
+        Activity activity = Activity.playing(responseTime + " ms - " + df.format(memUsed) + " MB / " + df.format(memTotal) + " MB");
         jda.getPresence().setActivity(activity);
     }
 }
