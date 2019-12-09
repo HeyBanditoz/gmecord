8802c8d180b7e4f12ee16e2f8bf2ddcb93c68ee1
HeyBanditoz <banditoz@protonmail.com>
2019-12-09 08:31:40 -0700

    Make some things in activity static

--- src/main/java/io/banditoz/gmecord/tasks/ActivityTimerTask.java
+++ src/main/java/io/banditoz/gmecord/tasks/ActivityTimerTask.java
@@ -8,16 +8,17 @@ import java.text.DecimalFormat;
 import java.util.TimerTask;
 
 public class ActivityTimerTask extends TimerTask {
+    private static final Runtime RUNTIME = Runtime.getRuntime();
+    private static final double TOTAL_MEMORY = RUNTIME.maxMemory() / (1024.0 * 1024.0);
+
     @Override
     public void run() {
         JDA jda = Bot.getJda();
         DecimalFormat df = new DecimalFormat(".#"); // round to one decimal place
 
         long responseTime = jda.getGatewayPing();
-        Runtime runtime = Runtime.getRuntime();
-        double memUsed = (runtime.totalMemory() - runtime.freeMemory()) / (1024.0 * 1024.0);
-        double memTotal = runtime.maxMemory() / (1024.0 * 1024.0);
-        Activity activity = Activity.playing(responseTime + " ms - " + df.format(memUsed) + " MB / " + df.format(memTotal) + " MB");
+        double memUsed = (RUNTIME.totalMemory() - RUNTIME.freeMemory()) / (1024.0 * 1024.0);
+        Activity activity = Activity.playing(responseTime + " ms - " + df.format(memUsed) + " MB / " + df.format(TOTAL_MEMORY) + " MB");
         jda.getPresence().setActivity(activity);
     }
 }
