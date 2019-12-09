50c23f2964e08bd4ddc2ecd7981becfe78488faf
HeyBanditoz <banditoz@protonmail.com>
2019-12-09 08:28:11 -0700

    Give 400 if it isn't JSON

--- src/main/java/io/banditoz/gmecord/web/MessageHandler.java
+++ src/main/java/io/banditoz/gmecord/web/MessageHandler.java
@@ -1,5 +1,6 @@
 package io.banditoz.gmecord.web;
 
+import com.google.gson.JsonParseException;
 import io.banditoz.gmecord.DiscordMessageCreator;
 import io.banditoz.gmecord.DiscordMessageSender;
 import io.banditoz.gmecord.SettingsManager;
@@ -17,18 +18,23 @@ public class MessageHandler {
         try {
             ctx.contentType("text/plain");
             GroupmeMessage message = SerializerDeserializer.deserializeMessageGivenString(ctx.body());
-            if (message.getName() == null || message.getText() == null) {
+            if (message == null || (message.getName() == null || message.getText() == null)) {
                 logger.warn("Bad request, message body: " + ctx.body());
                 ctx.status(Response.SC_BAD_REQUEST);
                 ctx.result("400 Bad Request\n");
                 return;
             }
-            logger.debug("Message inbound... Message: " +  message + " Raw body: " + ctx.body());
+            logger.debug("Message inbound... Message: " + message + " Raw body: " + ctx.body());
             if (!message.getName().equals(SettingsManager.getInstance().getSettings().getBotName())) {
                 lastUser = message.getName();
                 logger.debug("Setting last user to " + lastUser);
             }
             DiscordMessageSender.sendMessageToDiscord(new DiscordMessageCreator(message, message.getSystem()).getMessage());
+        } catch (JsonParseException e) {
+            logger.warn("Bad JSON from webserver. ", e);
+            ctx.status(Response.SC_BAD_REQUEST);
+            ctx.result(e.toString() + "\n");
+            return;
         } catch (Exception e) {
             DiscordMessageCreator creator = new DiscordMessageCreator("Exception thrown on Javalin post! `" + e.toString() + "`", true);
             DiscordMessageSender.sendMessageToDiscord(creator.getMessage());
