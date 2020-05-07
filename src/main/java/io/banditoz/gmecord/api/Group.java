a6ff611f26fb93fd903f5c9b25bb87f965278405
HeyBanditoz <banditoz@protonmail.com>
2020-05-07 14:55:51 -0600

    Ignore muted_until field from group object

--- src/main/java/io/banditoz/gmecord/api/Group.java
+++ src/main/java/io/banditoz/gmecord/api/Group.java
@@ -1,9 +1,11 @@
 package io.banditoz.gmecord.api;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+@JsonIgnoreProperties("muted_until")
 public class Group {
     @Override
     public String toString() {
